fdd0e4297e58d189b779114c7d678a55dbb38fc1
KoLobOK <webkolobok@mail.ru>
2023-01-27 17:58:08 +0300

    feat: create Portfolio component

--- movies-explorer-frontend/src/components/Portfolio/Portfolio.js
+++ movies-explorer-frontend/src/components/Portfolio/Portfolio.js
@@ -1,10 +1,43 @@
 import './Portfolio.css';
 
 const Portfolio = () => {
+    const projects = [
+        {
+            title: 'Статичный сайт',
+            reference: 'https://kolob-ok.github.io/How-to-learn'
+        },
+        {
+            title: 'Адаптивный сайт',
+            reference: 'https://kolob-ok.github.io/Russian-travel'
+        },
+        {
+            title: 'Одностраничное приложение',
+            reference: 'https://github.com/KoLob-OK/react-mesto-api-full'
+        },
+    ];
+
     return (
-        <div className='portfolio'>
-            <p>Это Portfolio-component</p>
-        </div>
+        <section className='section__block section__block_type_portfolio'>
+            <h3 className='portfolio__title'>Портфолио</h3>
+            <ul className='portfolio__projects'>
+                {projects.map((project) => (
+                    <li className='portfolio__project'>
+                        <a className='portfolio__link'
+                           href={project.reference}
+                           target='_blank'>
+                            {project.title}
+                            {/*если нужен клик по всей строке*/}
+                            {/*<span className='portfolio__link-icon'>↗</span>*/}
+                        </a>
+                        <a className='portfolio__link-icon'
+                           href={project.reference}
+                           target='_blank'>
+                            ↗
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 };
 
